eda5e92108d027aacdd8fbf85db7ed90c6026f08
Rafael Jesus <rafael.jesus17@hotmail.com>
2023-09-18 19:50:54 -0300

    form

--- resources/views/lancamento/form.blade.php
+++ resources/views/lancamento/form.blade.php
@@ -39,33 +39,54 @@
         <label for="id_centro_custo" class="form-label">Centro de Custo*</label>
         <select id="id_centro_custo" class="form-select" required>
             <option value="">Escolha...</option>
-            <option value="">...</option>
+                @foreach ($centrosDeCusto::orderBy('centro_custo')->get() as $centro )
+                    <option value="{{$centro->id_centro_custo}}"
+                        @selected(
+                            (
+                                $lancamento &&
+                                $lancamento->id_centro_custo == $centro->id_centro_custo
+                            )
+                            ||
+                            old('id_centro_custo') == $centro->id_centro_custo
+                        )
+                    >
+                        {{ $centro->centro_custo}}
+                    </option>
+                @endforeach
         </select>
     </div>
     <div class="col-md-4">
         <label class="form-label" for="valor">Valor*</label>
-        <input class="form-control" type="number" id="valor" name="valor" value="" required>
+        <input class="form-control" type="number" id="valor" name="valor"
+         value="{{
+            $lancamento ?
+            $lancamento->valor :
+            old('valor')
+          }}"
+         required>
     </div>
-    <div class="col-md-4">
-        <label class="form-label" for="descricao">Descrição*</label>
-        <input class="form-control" type="text" id="descricao" name="descricao" value="" required>
-    </div>
-
-    <div class=" col-md-3">
-        <label for="anexo" class="form-label">
+    <div class="col-md-3">
+        <label class="form-label" for="anexo" >
             Anexo
         </label>
-        <input type="file" class="form-control" name="anexo" id="anexo"
-        value="{{ old('anexo') }}"   >
-
-
+        <input class="form-control" type="file"  name="anexo" id="anexo"
+        value="{{ old('anexo') }}">
     </div>
 
-
-
-
+    <div class="col-md-12">
+        <label class="form-label" for="descricao">Descrição*</label>
+        <input class="form-control" type="text" id="descricao" name="descricao"
+        value="{{
+            $lancamento ?
+            $lancamento->descricao :
+            old('descricao')
+          }}"
+          required>
+    </div>
     <div class="col-md-2 offset-md-9">
-        <input class="btn btn-primary" type="submit" value=" Cadastrar  Atualizar">
+        <input class="btn btn-primary" type="submit"
+         value="{{ $lancamento ? 'Atualizar' : 'Cadastrar' }}"
+         >
     </div>
 
 
